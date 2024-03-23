e9d1c1ecbee75db81706d54452308b1bf5790817
PeiYun <ollylee8520@gmail.com>
2024-03-23 14:45:56 -0600

    update add item for ingrdient

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrl: './shopping-edit.component.css'
+  styleUrl: './shopping-edit.component.css',
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput', { static: false }) nameInput: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInput: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
+  onAddItem() {
+    const ingName = this.nameInput.nativeElement.value;
+    const ingMount = this.amountInput.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingMount);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
